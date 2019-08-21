ca4ce2164351641ef7d0bc815bd75462bb4f8ce5
Domeniqque Dylluar <domeniqque@hotmail.com>
2019-08-21 06:26:19 -0400

    chore: add express, nodemon &  sucrase

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import routes from "./routes";
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.routes();
+    this.middlewares();
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+
+  middlewares() {}
+}
+
+export default new App().server;
